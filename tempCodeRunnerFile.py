22ddf523d096d6dcdd6db438f6f88daafe72511d
anaquinterog <ana.quinterog@udem.edu>
2024-05-05 16:31:38 -0600

    Second commit: uploading images and codes

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+image1 = cv2.imread('/Users/anabi/Documents/GitHub/HW10_ObjectDetection/images/first_image.png')
+image2 = cv2.imread('/Users/anabi/Documents/GitHub/HW10_ObjectDetection/images/IMAGE_BASE.png')
